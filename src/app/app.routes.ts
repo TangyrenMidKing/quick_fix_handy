c78ebd858cff9b48c1224ee00cbf21220c5b0fe6
Shibo <d1029433728@gmail.com>
2025-01-21 16:29:14 -0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-// import { ServiceComponent } from './service/service.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-    // { path: 'service', component: ServiceComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
 ];
